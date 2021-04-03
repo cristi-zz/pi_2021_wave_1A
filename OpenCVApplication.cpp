47efa4d0b5c48a63989067e51bc40a5321c67942
Bogdan <bogdan.bl15@yahoo.com>
2021-04-03 18:05:37 +0300

    Generare ll, lh, hl, hh si reconstructie

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -1,8 +1,11 @@
 // OpenCVApplication.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
 #include "common.h"
+#include <stdio.h>
+#include <vector>
+
+//#define ASSERTS_ON
 
 
 void testOpenImage()
@@ -64,6 +67,228 @@ void testColor2Gray()
 	}
 }
 
+bool inline isPowerOfTwo(const int number)
+{
+	return ((number & (number - 1)) == 0);
+}
+
+std::vector<double> getLowVector(const std::vector<double>& arr)
+{
+	std::vector<double> result(arr.size() / 2);
+	for (auto i = 0; i < arr.size() / 2; i++)
+	{
+		result[i] = (arr[2 * i] + arr[2 * i + 1]) / 2.0f;
+	} 
+	return result;
+}
+
+std::vector<double> getHighVector(const std::vector<double>& arr)
+{
+	std::vector<double> result(arr.size() / 2);
+	for (auto i = 0; i < arr.size() / 2; i++)
+	{
+		result[i] = (arr[2 * i] - arr[2 * i + 1]) / 2.0f;
+	}
+	return result;
+}
+
+std::vector<double> getLowVectorUpSample(const std::vector<double>& arr)
+{
+	std::vector<double> result(2 * arr.size());;
+	for (int i=0; i < result.size(); i++)
+	{
+		result[i] = arr[i / 2];
+	}
+	return result;
+}
+
+std::vector<double> getHighVectorUpSample(const std::vector<double>& arr)
+{
+	const int h[] = { 1, -1 };
+	std::vector<double> result(2 * arr.size());;
+
+	for (int i = 0; i < result.size(); i++)
+	{
+		result[i] = arr[i / 2] * h[i % 2];
+	}
+	return result;
+}
+
+std::vector<Mat_<double>> splitImage(Mat_<double> src)
+{
+	Mat_<double> l = Mat_<double>(src.rows / 2, src.cols);
+	Mat_<double> h = Mat_<double>(src.rows / 2, src.cols);
+
+	Mat_<double> ll = Mat_<double>(src.rows / 2, src.cols / 2);
+	Mat_<double> lh = Mat_<double>(src.rows / 2, src.cols / 2);
+	Mat_<double> hl = Mat_<double>(src.rows / 2, src.cols / 2);
+	Mat_<double> hh = Mat_<double>(src.rows / 2, src.cols / 2);
+
+	for (auto j = 0; j < src.cols; j++)
+	{
+		std::vector<double> copy(src.rows);
+
+		for (auto i = 0; i < src.rows; i++)
+		{
+			copy[i] = src[i][j];
+		}
+
+		auto low = getLowVector(copy);
+		auto high = getHighVector(copy);
+
+		for (auto i = 0; i < low.size(); i++)
+		{
+			l[i][j] = low[i];
+			h[i][j] = high[i];
+		}
+	}
+
+
+	for (auto i = 0; i < src.rows / 2; i++)
+	{
+		std::vector<double> copy_low(src.cols);
+		std::vector<double> copy_high(src.cols);
+
+		for (auto j = 0; j < src.cols; j++)
+		{
+			copy_low[j] = l[i][j];
+			copy_high[j] = h[i][j];
+		}
+
+		auto low_low = getLowVector(copy_low);
+		auto high_low = getHighVector(copy_low);
+
+		auto low_high = getLowVector(copy_high);
+		auto high_high = getHighVector(copy_high);
+
+		for (auto j = 0; j < low_low.size(); j++)
+		{
+			ll[i][j] = low_low[j];
+			lh[i][j] = low_high[j];
+
+			hl[i][j] = high_low[j];
+			hh[i][j] = high_high[j];
+		}
+	}
+
+	return std::vector<Mat_<double>>{ll, lh, hl, hh};
+
+}
+
+Mat_<double> reconstructImage(Mat_<double> ll, Mat_<double> lh, Mat_<double> hl, Mat_<double> hh)
+{
+	const int32_t rows = ll.rows;
+	const int32_t cols = ll.cols;
+
+	Mat_<double> l(rows, 2 * cols);
+	Mat_<double> h(rows, 2 * cols);
+
+	Mat_<double> result(2 * rows, 2 * cols);
+
+	for (auto i = 0; i < rows; i++)
+	{
+		std::vector<double> copy_low_low(cols);
+		std::vector<double> copy_low_high(cols);
+
+		std::vector<double> copy_high_low(cols);
+		std::vector<double> copy_high_high(cols);
+
+		for (auto j = 0; j < cols; j++)
+		{
+			copy_low_low[j] = ll[i][j];
+			copy_low_high[j] = lh[i][j];
+
+			copy_high_low[j] = hl[i][j];
+			copy_high_high[j] = hh[i][j];
+		}
+
+		auto low_low_sample = getLowVectorUpSample(copy_low_low);
+		auto low_high_sample = getHighVectorUpSample(copy_low_high);
+
+		auto high_low_sample = getLowVectorUpSample(copy_high_low);
+		auto high_high_sample = getHighVectorUpSample(copy_high_high);
+
+		for (auto j = 0; j < low_low_sample.size(); j++)
+		{
+			l[i][j] = low_low_sample[j] + low_high_sample[j];
+			h[i][j] = high_low_sample[j] + high_high_sample[j];
+		}
+	}
+
+	for (int j = 0; j < 2 * cols; j++)
+	{
+		std::vector<double> copy_low(rows);
+		std::vector<double> copy_high(rows);
+
+		for (auto i = 0; i < rows; i++)
+		{
+			copy_low[i] = l[i][j];
+			copy_high[i] = h[i][j];
+		}
+
+		auto low_sample = getLowVectorUpSample(copy_low);
+		auto high_sample = getHighVectorUpSample(copy_high);
+
+		for (auto i = 0; i < low_sample.size(); i++)
+		{
+			result[i][j] = low_sample[i] + high_sample[i];
+		}
+	}
+
+	return result;
+}
+
+Mat_<double> reconstructImage(std::vector<Mat_<double>> parts)
+{
+
+#ifdef ASSERTS_ON
+	assert(parts.size() == 4);
+	for (auto& i : parts)
+	{
+		for (auto& j : parts)
+		{
+			assert(i.cols == j.cols);
+			assert(i.rows == j.rows);
+		}
+	}
+#endif // ASSERTS_ON
+
+	return reconstructImage(parts[0], parts[1], parts[2], parts[3]);
+}
+
+void splitAndReconstruct()
+{
+	char fname[MAX_PATH] = {};
+	while (openFileDlg(fname))
+	{
+		Mat_<double> src = imread(fname, IMREAD_GRAYSCALE);
+
+#ifdef ASSERTS_ON
+		assert(isPowerOfTwo(src.rows));
+		assert(isPowerOfTwo(src.cols));
+#endif // ASSERTS_ON
+
+		auto parts = splitImage(src);
+
+		const Mat_<uint8_t> ll = parts[0];
+		const Mat_<uint8_t> lh = parts[1];
+		const Mat_<uint8_t> hl = parts[2];
+		const Mat_<uint8_t> hh = parts[3];
+
+		imshow("LL", ll);
+		imshow("LH", lh);
+		imshow("HL", hl);
+		imshow("HH", hh);
+
+		Mat_<uint8_t> reconstructed = reconstructImage(parts);
+		imshow("Reconstructed", reconstructed);
+
+		Mat_<uint8_t> original = src;
+		imshow("Original", original);
+
+		cv::waitKey(0);
+	}
+}
 
 int main()
 {
@@ -73,22 +298,12 @@ int main()
 		system("cls");
 		destroyAllWindows();
 		printf("Menu:\n");
-		printf(" 1 - Basic image opening...\n");
-		printf(" 2 - Open BMP images from folder\n");
-		printf(" 3 - Color to Gray\n");
-		printf(" 0 - Exit\n\n");
-		printf("Option: ");
+		printf("1 - Split and reconstruct an image\n");
 		scanf("%d",&op);
 		switch (op)
 		{
 			case 1:
-				testOpenImage();
-				break;
-			case 2:
-				testOpenImagesFld();
-				break;
-			case 3:
-				testColor2Gray();
+				splitAndReconstruct();
 				break;
 		}
 	}
